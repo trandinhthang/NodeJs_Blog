828abe0fadce7645cc7a85bf95cb7a4dd9c0ffd0
trandinhthang <thang.tran.5243817@gmail.com>
2021-02-24 12:18:56 +0700

    method post

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// use midleware
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.json());
+
 // HTTP logger
 app.use(morgan('combined'))
 
@@ -15,16 +22,25 @@ app.engine('hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-// route home
+// route home // app.METHOD(PATH, HANDLER)
 app.get('/', (req, res) => { 
     res.render('home');
-})
+});
+// route search
+app.get('/tim-kiem', (req, res) => {
+  res.render('search');
+});
+// post data search
+app.post('/tim-kiem', (req, res) => {
+  console.log(req.body)
+  res.send('');
+});
 //route news
 app.get('/tin-tuc', (req, res) => { 
     res.render('news');
-})
+});
 
 // listen 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
